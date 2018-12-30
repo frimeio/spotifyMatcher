46437179693a0d69e8f9fc0073f5dc0b73813a6e
Ugur <ugurkoysuren@gmail.com>
2018-12-30 22:44:08 +0100

    init of matched component

---
+++ src/app/matched/matched.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {SpotifyService} from '../spotify/spotify.service';
+import {HttpHeaders} from '@angular/common/http';
+
+@Component({
+  selector: 'app-matched',
+  templateUrl: './matched.component.html',
+  styleUrls: ['./matched.component.css']
+})
+export class MatchedComponent implements OnInit {
+  newReleases : any[] =[];
+  preloading: boolean = true;
+  message: string;
+  error: boolean = false;
+  data: string;
+
+  headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json',
+    'Authorization': 'Bearer 2 '+ this.data
+  });
+
+
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public _spotifyService: SpotifyService
+  ) {
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.data = params['access_token'];
+      console.log(this.data);
+    });
+
+    this.headers.set('Authorization', 'Bearer3 '+ this.data);
+
+
+    this._spotifyService.getNewReleases(this.data)
+      .subscribe(
+        (data: any) =>{
+          this.newReleases = data;
+          this.preloading = false;
+          console.log(this.newReleases);
+        },(error)=>{
+          this.preloading = false;
+          this.error = true;
+          this.message = error.error.error.message;
+          console.log(error.error.error.message);
+        });
+
+
+  }
+  ngOnInit() {
+
+    this._spotifyService.getNewReleases(this.headers)
+      .subscribe(
+        (data: any) =>{
+          this.newReleases = data;
+          this.preloading = false;
+          console.log(this.newReleases);
+        },(error)=>{
+          this.preloading = false;
+          this.error = true;
+          this.message = error.error.error.message;
+          console.log(error.error.error.message);
+        });
+
+
+this._spotifyService.getArtist('eminem', this.headers)
+  .subscribe(
+    (data: any) =>{
+      this.newReleases = data;
+      this.preloading = false;
+      console.log(this.newReleases);
+    },(error)=>{
+      this.preloading = false;
+      this.error = true;
+      this.message = error.error.error.message;
+      console.log(error.error.error.message);
+    });
+
+}  }
