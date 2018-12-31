000c8b088441326a05dcf8710dd907ec51ccc184
Ugur <ugurkoysuren@gmail.com>
2018-12-31 01:04:49 +0100

    clean state

--- src/app/matched/matched.component.ts
+++ src/app/matched/matched.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {SpotifyService} from '../spotify/spotify.service';
-import {HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-matched',
@@ -9,18 +8,10 @@ import {HttpHeaders} from '@angular/common/http';
   styleUrls: ['./matched.component.css']
 })
 export class MatchedComponent implements OnInit {
-  newReleases : any[] =[];
-  preloading: boolean = true;
+  topArtist: any;
   message: string;
-  error: boolean = false;
+  error: Boolean;
   data: string;
-
-  headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json',
-    'Authorization': 'Bearer 2 '+ this.data
-  });
-
-
-
   constructor(
     private activatedRoute: ActivatedRoute,
     public _spotifyService: SpotifyService
@@ -30,24 +21,16 @@ export class MatchedComponent implements OnInit {
       this.data = params['access_token'];
       console.log(this.data);
     });
-
-    this.headers.set('Authorization', 'Bearer3 '+ this.data);
-
-
     this._spotifyService.getTopTrack(this.data)
       .subscribe(
-        (data: any) =>{
-          this.newReleases = data;
-          this.preloading = false;
-          console.log(this.newReleases);
-        },(error)=>{
-          this.preloading = false;
+        (data: any) => {
+          this.topArtist = data;
+        }, (error) => {
           this.error = true;
           this.message = error.error.error.message;
           console.log(error.error.error.message);
         });
-
-
+    console.log(this.topArtist);
   }
   ngOnInit() {
   }
